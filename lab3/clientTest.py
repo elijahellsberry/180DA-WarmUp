e3f056ad961e2c48998e124d4bf7f61066b0cc93
Elijah Ellsberry <elijah.ellsberry1@gmail.com>
2021-11-03 13:48:10 -0700

    transferring to rpi

--- lab3/clientTest.py
+++ lab3/clientTest.py
@@ -1,8 +1,8 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.224.76', 8080))
+client.connect(('192.168.179.76', 8080))
 message = "I am CLIENT\n"
-client.sendto(message.encode(), ('192.168.224.76', 8080))
+client.sendto(message.encode(), ('192.168.179.76', 8080))
 data = client.recv(4096)
 from_server = data.decode()
 client.close()
