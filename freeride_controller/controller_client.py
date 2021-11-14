a37b40ae4abc885057cee2985f5650122f9e84ce
Elijah Ellsberry <elijah.ellsberry1@gmail.com>
2021-11-14 04:26:50 -0800

    a

--- freeride_controller/controller_client.py
+++ freeride_controller/controller_client.py
@@ -1,13 +1,13 @@
 import socket
 
-server_address = "D4-3B-04-97-DA-5F"
-port = 0
-
+server_address = 'd4:3b:04:97:da:5f' # change this to your host device's mac address
+port = 6
 
 client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 client.connect((server_address, port))
 message = "I am CLIENT\n"
-client.sendto(message.encode(), (server_address, port))
+#client.sendto(message.encode(), (server_address, port))
+client.send(bytes(message, 'UTF-8'))
 data = client.recv(4096)
 from_server = data.decode()
 client.close()
