419b8ea6764fe37aa9d1de5fb59d5445e420d648
elijahellsberry <73855252+elijahellsberry@users.noreply.github.com>
2021-11-13 16:59:29 -0800

    changed client code to use pybluez on linux

--- freeride_controller/controller_client.py
+++ freeride_controller/controller_client.py
@@ -1,13 +1,15 @@
-import socket
+import bluetooth
 
 server_address = "D4-3B-04-97-DA-5F"
-port = 0
+port = 6
 
 
-client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+#client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 client.connect((server_address, port))
 message = "I am CLIENT\n"
-client.sendto(message.encode(), (server_address, port))
+client.send(message)
+#client.sendto(message.encode(), (server_address, port))
 data = client.recv(4096)
 from_server = data.decode()
 client.close()
