a0e086c994836d0f17441b5b7b421aba83c8abf2
Elijah Ellsberry <elijah.ellsberry1@gmail.com>
2021-11-17 04:42:36 -0800

    controller client test code

--- freeride_controller/controller_client.py
+++ freeride_controller/controller_client.py
@@ -1,16 +1,25 @@
-import bluetooth
+#import bluetooth
+import socket
+import time
 
 server_address = 'd4:3b:04:97:da:5f'
 port = 6
 
 
-#client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-client.connect((server_address, port))
-message = "I am CLIENT\n"
-client.send(message)
+client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+#client = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+client.connect(('d4:3b:04:97:da:5f', 5))
+while True:
+    message = "Hello"
+    client.send(message.encode())
+    print(str(message))
+    
+# client.send(str(time.time_ns()).encode())
+# client.close()
+
+
 #client.sendto(message.encode(), (server_address, port))
-data = client.recv(4096)
-from_server = data.decode()
-client.close()
-print(from_server)
+#data = client.recv(4096)
+#from_server = data.decode()
+#client.close()
+#print(from_server)
