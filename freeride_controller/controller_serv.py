c537089662fcd60877b2909bc1365783a0c56855
elijahellsberry <73855252+elijahellsberry@users.noreply.github.com>
2021-11-17 04:34:27 -0800

    bluetooth host code

--- freeride_controller/controller_serv.py
+++ freeride_controller/controller_serv.py
@@ -2,28 +2,41 @@ import socket
 import json
 import time
 
-server_address = 'd4:3b:04:97:da:5f'
-port = 5
-
 # Initialization to add RFCOMM protocol to endpoint
 serv = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
 # Assigns a port for the server that listens to clients connecting to this port
-serv.bind((server_address, port))
-serv.listen(1)
+serv.bind(('d4:3b:04:97:da:5f', 5))
+serv.listen()
+prev_time = 0
 while True:
     client, addr = serv.accept()
-    from_client = ''
     while True:
         data = client.recv(4096)
+        curr_time = time.time_ns()
         if not data: break 
         from_client_string = data.decode()
-        print(type(from_client_string))
-        from_client = json.load(from_client_string)
-        recv_time = time.time_ns()
-        sent_time = int(from_client["time"])
-        latency = recv_time - sent_time
-        print(str(latency) + " ms")
+        from_client = json.loads(from_client_string)
+
+        packet_time = (curr_time - prev_time) / (10**6)
+        prev_time = curr_time
+
+        print(str(packet_time) + " ms")
+
+
+
+
+        # data = client.recv(4096)
+        # curr_time = time.time_ns()
+        # if not data: break
+        # from_client = data.decode()
+        # packet_time = (curr_time - prev_time) / (10**6)
+        # prev_time = curr_time
+        # print(str(packet_time) + "ms")
+
+
+
+
 
         #from_client += data
         #print(from_client)
