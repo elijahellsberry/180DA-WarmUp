62fe4e7a6648d81869b315589cedc337436e7993
Elijah Ellsberry <elijah.ellsberry1@gmail.com>
2021-11-17 04:37:57 -0800

    junk changes need to merge with latest

--- freeride_controller/controller_serv.py
+++ freeride_controller/controller_serv.py
@@ -2,33 +2,29 @@ import socket
 import json
 import time
 
-<<<<<<< HEAD
-server_address = 'd4:3b:04:97:da:5f' # change this to your host device's mac address
-port = 0
-=======
 server_address = 'd4:3b:04:97:da:5f'
 port = 5
->>>>>>> 7e4a5fd61aeb8e4d8c737377e2b5c28aeb1de1f5
 
 # Initialization to add RFCOMM protocol to endpoint
 serv = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
 # Assigns a port for the server that listens to clients connecting to this port
-serv.bind((server_address, port))
-serv.listen(1)
+serv.bind(('5c:fb:3a:65:d7:80', 5))
+serv.listen(5)
 while True:
     client, addr = serv.accept()
     from_client = ''
     while True:
         data = client.recv(4096)
-        if not data: break 
-        from_client_string = data.decode()
-        print(type(from_client_string))
-        from_client = json.load(from_client_string)
         recv_time = time.time_ns()
-        sent_time = int(from_client["time"])
-        latency = recv_time - sent_time
-        print(str(latency) + " ms")
+        if not data: break
+        from_client = data.decode()
+        sent_time = int(from_client)
+        #from_client_str = data.decode()
+        #from_client = json.loads(data.decode())
+        #sent_time = int(from_client["time"])
+        latency = (recv_time - sent_time) / (10**6)
+        print("Recieved at: " + str(recv_time) + " Transmit at: " + str(sent_time) + " Latency: " + str(latency))
 
         #from_client += data
         #print(from_client)
@@ -38,4 +34,3 @@ while True:
     client.close()
     print('client disconnected')
     break
-
